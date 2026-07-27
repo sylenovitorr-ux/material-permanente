9815a5ceeb9ac74da1db6ac76489efdf723dba1f
sylenovitorr-ux <sylenovitorr@gmail.com>
2026-07-27 13:01:16 -0400

    Adiciona arquivos do aplicativo integral: app-atual/sw.js

---
+++ app-atual/sw.js
@@ -0,0 +1,27 @@
+const CACHE = "material-permanente-github-integral-v2";
+const ASSETS = ["./", "./app.js", "./dados.json", "./styles.css", "./manifest.webmanifest", "./favicon.svg", "./icons/icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./"))),
+  );
+});
